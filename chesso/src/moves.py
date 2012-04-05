7f3d1d899e67c233705ac6e3a617f7849640589f
uri <uri@uri2.london.home>
2012-04-05 11:45:25 +0300

    Complete movements

--- chesso/src/moves.py
+++ chesso/src/moves.py
@@ -9,18 +9,19 @@ from location import Loc
 from pieces import Shape, Colors, Piece, opositeColor, ShapeTraits, PromotableShapes
 
 class Move(object):
-    def __init__(self, src, dst):
+    def __init__(self, pc, src, dst):
+        self.pc = pc
         self.src = src
         self.dst = dst
     def __repr__(self):
-        return "{0}=>{1}".format(self.src,self.dst)
+        return "{0}: {1}=>{2}".format(self.pc,self.src,self.dst)
         
 class PromotionMove(Move):
-    def __init__(self, src, dst, newPiece):
-        Move.__init__(self,src, dst)
+    def __init__( self, pc, src, dst, newPiece ):
+        Move.__init__( self, pc, src, dst )
         self.newPiece = newPiece
     def __repr__(self):
-        return "{0}=>{1} promoted to {2}".format(self.src,self.dst,self.newPiece)
+        return "{0}: {1}=>{2} promoted to {3}".format( self.pc, self.src, self.dst, self.newPiece )
 
 def pawnBeforePromotion(sq):
     piece = sq.piece
@@ -71,34 +72,35 @@ def possibleMovePawn(board,sq):
                     have = len( board.force(shape,piece.color) )
                     maximum = shtrt.maximum
                     if have < maximum:
-                        moves.append( PromotionMove(src, dst, Piece(shape, piece.color)) )
+                        moves.append( PromotionMove(piece, src, dst, Piece(shape, piece.color)) )
             else:
-                moves.append( Move(src, dst) )
+                moves.append( Move(piece, src, dst) )
     if pawnOnStart(sq):
         f = forward(src, piece.color)
         if (board[f] == None):
             df = forward( f, piece.color )
             if board[df] == None:
-                m = Move( src, df )
+                m = Move( piece, src, df )
                 moves.append(m)
     return moves
+
+
+directionsDict = {
+    Shape.ROOK: [ Loc.north, Loc.south, Loc.east, Loc.west ],
+    Shape.BISHOP: [ Loc.nw, Loc.ne, Loc.sw, Loc.se ],
+    Shape.QUEEN: [ Loc.nw, Loc.north, Loc.ne, Loc.west, Loc.east, Loc.sw, Loc.south, Loc.se ],
+    Shape.KNIGHT: [ Loc.nnw, Loc.nne, Loc.nee, Loc.see, Loc.sse, Loc.ssw, Loc.sww, Loc.nww ],
+    Shape.KING: [  Loc.nw, Loc.north, Loc.ne, Loc.west, Loc.east, Loc.sw, Loc.south, Loc.se ],
+    }
 
 
 def possibleMoveRank(board,sq):
     assert isinstance(board, Board)
     assert isinstance(sq, Square)
     assert sq.piece.shape in [Shape.ROOK, Shape.BISHOP, Shape.QUEEN]
-
     moves = []
     src = sq.loc
     piece = sq.piece
-
-    directionsDict = {
-        Shape.ROOK: [ Loc.north, Loc.south, Loc.east, Loc.west ],
-        Shape.BISHOP: [ Loc.nw, Loc.ne, Loc.sw, Loc.se ],
-        Shape.QUEEN: [ Loc.nw, Loc.north, Loc.ne, Loc.west, Loc.east, Loc.sw, Loc.south, Loc.se ]
-        }
-    
     directions = directionsDict[ piece.shape ]
 
     for direction in directions:
@@ -108,7 +110,7 @@ def possibleMoveRank(board,sq):
             if not dst:
                 break                
             elif not board[dst] or board[dst].color == opositeColor(piece.color):
-                moves.append( Move(src, dst) )
+                moves.append( Move(piece, src, dst) )
                 if board[dst]:
                     break
             else:
@@ -117,22 +119,38 @@ def possibleMoveRank(board,sq):
         pass
     return moves
 
-def nop(board,sq):
-    return [] 
 
+
+def possibleMoveShort(board,sq):
+    assert isinstance(board, Board)
+    assert isinstance(sq,Square)
+    assert sq.piece.shape in [ Shape.KNIGHT, Shape.KING ]
+    moves = []    
+    for fn in directionsDict[ sq.piece.shape ]:
+        dst = fn(sq.loc)
+        if dst and (not board[dst] or board[dst].color == opositeColor(sq.piece.color)):
+            moves.append( Move(sq.piece, sq.loc,dst) )
+    return moves
+    
+    
+def nop(board,sq):
+    return []
+
+movedict = {
+    Shape.PAWN: possibleMovePawn,
+    Shape.ROOK: possibleMoveRank,
+    Shape.BISHOP: possibleMoveRank,
+    Shape.QUEEN: possibleMoveRank,
+    Shape.KING: possibleMoveShort,
+    Shape.KNIGHT: possibleMoveShort,
+    }
+    
 def possibleMove(board, sq):
     assert isinstance(board,Board)
     assert isinstance(sq, Square)
     src = sq.loc
     piece = board[src]
-    movedict = {
-        Shape.PAWN: possibleMovePawn,
-        Shape.ROOK: possibleMoveRank,
-        Shape.BISHOP: possibleMoveRank,
-        Shape.QUEEN: possibleMoveRank,
-        Shape.KING: nop,
-        Shape.KNIGHT: nop
-        }
+
     movefn = movedict[piece.shape]
     return movefn(board,sq)
     
@@ -140,20 +158,17 @@ def possibleMove(board, sq):
 def executeMove( board, move ):
     assert isinstance(board,Board)
     assert isinstance(move, Move)
-    
-    
 
 
 if __name__ == '__main__':
     b = Board()
-    ''''b.readFen('rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2')'''
-    b.readFen('r1nb1kbn/rPppPppp/p7/8/2Q2P2/6N1/R5PP/2N2K2')
+    b.readFen('r1nb1kbn/rPppPppp/p7/8/2Q2P2/6N1/R5PP/2N2K2 w')
     print b
     
-    pmvs = []
-    whites = b.army(Colors.WHITE)
-    for sq in whites:
-        pmvs += possibleMove(b,sq)
+    mvs = []
+    army = b.army( b.nextmove )
+    for sq in army:
+        mvs += possibleMove( b, sq )
         
-    for mv in pmvs:
+    for mv in mvs:
         print mv
