130c337753e0c3be24c51d3518e9c48cb32e6ba1
uri <uri@uri2.london.home>
2012-04-05 11:44:22 +0300

    Horse movements

--- chesso/src/location.py
+++ chesso/src/location.py
@@ -17,6 +17,8 @@ class Loc(object):
         self.col = col
     def __repr__(self):
         return "({0},{1})".format(self.row, self.col)
+    def index(self):
+        return self.row*8 + self.col
     def north(self):
         return (self.row > 0) and Loc(self.row-1,self.col) or None
     def south(self):
@@ -33,6 +35,19 @@ class Loc(object):
         return (self.row < 7 and self.col > 0) and Loc(self.row+1, self.col-1) or None
     def se(self):
         return (self.row < 7 and self.col < 7) and Loc(self.row+1,self.col+1) or None
-    def index(self):
-        return self.row*8 + self.col
-
+    def nnw(self):
+        return self.north() and self.north().nw() or None
+    def nne(self):
+        return self.north() and self.north().ne() or None
+    def nee(self):
+        return self.east() and self.east().ne() or None
+    def see(self):
+        return self.east() and self.east().se() or None
+    def sse(self):
+        return self.south() and self.south().se() or None
+    def ssw(self):
+        return self.south() and self.south().sw() or None
+    def sww(self):
+        return self.west() and self.west().sw() or None
+    def nww(self):
+        return self.west() and self.west().nw() or None
