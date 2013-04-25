44fee6a66a6c30cb5118a31afac7b124751b11db
Uri London <uri@london.org.il>
2013-04-25 17:41:14 +0300

    HelloAppEngine: fall rectangle page for uploading blobs

--- HelloAppEngine/fallrec.py
+++ HelloAppEngine/fallrec.py
@@ -2,8 +2,10 @@
 """
 import os
 import jinja2
+import urllib
 from google.appengine.ext import blobstore
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import blobstore_handlers
 from utils import ReqHandler
 
 jinja_environment = jinja2.Environment( loader=jinja2.FileSystemLoader(os.path.dirname(__file__)) )
@@ -19,17 +21,38 @@ class MainPage(ReqHandler):
         self.w( template.render(context))
 
 
-class UploadHandler(ReqHandler):
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def get(self):
         pass
     
     def post(self):
-        pass
+        files = self.get_uploads('thefile')
+        info = files[0]
+        template = jinja_environment.get_template('fallrec-details.html')
+        context = {
+            'key': info.key(),
+            'content_type': info.content_type,
+            'filename': info.filename,
+            'size': info.size,
+            'textline': self.request.get('textline'),
+            'link': '/fallrec/srv/%s' % info.key(),            
+        }
+        self.response.out.write( template.render(context))
+        
     
 
+class SrvHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, resource):
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        self.send_blob(blob_info)
+
+
 routes = [
-    ('/fallrec', MainPage ),
+    ('/fallrec/?', MainPage ),
     ('/fallrec/upload', UploadHandler ),
+    ('/fallrec/srv/([^/]+)?', SrvHandler ),
+
     ]
   
 
